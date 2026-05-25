343fdfbdd65b8acdf907d3d4ccbb597131e09869
fuermosi314 <windows15518158171@163.com>
2026-05-25 14:16:36 +0800

    去掉 Google Fonts，改用系统字体提升国内加载速度

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,10 +14,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh-CN" className="scroll-smooth">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      </head>
       <body className="antialiased">{children}</body>
     </html>
   );
